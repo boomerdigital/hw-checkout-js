9a83edb689685036c6b776902bda4a37804297e9
Freddy Garcia <freddygsalinas@gmail.com>
2025-02-20 12:32:47 -0500

    Update Lambda services

--- packages/core/src/app/avalara-certificates/services/LambdaService.ts
+++ packages/core/src/app/avalara-certificates/services/LambdaService.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 import { Address } from '@bigcommerce/checkout-sdk';
-const API_URL = 'https://61oz9fx2d9.execute-api.us-east-2.amazonaws.com/Prod/';
+const API_URL = 'https://yx1041xohb.execute-api.us-east-2.amazonaws.com/Prod/';
 
 export const fetchToken = async (): Promise<string | null> => {
     try {
-        const response = await fetch('/customer/current.jwt?app_client_id=npqb1dowfj7yeh7d1fqwhcod7wqs6al', {
+        const response = await fetch('/customer/current.jwt?app_client_id=bzhkzdt0f7vrrg92o4iym8rxvd872qj', {
             method: 'GET',
             headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
         });
